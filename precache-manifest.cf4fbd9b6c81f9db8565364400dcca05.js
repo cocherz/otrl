414614a073f7edb2a51c0800f56af6ab20ed79a7
Maxwell <maxwell.cochrane+github.com>
2022-04-26 01:19:21 +0100

    first tiees a chareemeeee

---
+++ precache-manifest.cf4fbd9b6c81f9db8565364400dcca05.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f627112556db3bef6d13",
+    "url": "/static/js/main.f6271125.chunk.js"
+  },
+  {
+    "revision": "6197f794f3d209bbeb95",
+    "url": "/static/js/1.6197f794.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a18373ae74f18e53ecd552fd9b1c16b6",
+    "url": "/index.html"
+  }
+];
